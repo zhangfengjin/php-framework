b9f84a705d8981ebb12cecbdc0f81080b501a299
zhangfengjin <793665131@qq.com>
2017-12-10 15:34:32 +0800

    demo

--- example/demo.php
+++ example/demo.php
@@ -3,12 +3,15 @@
 require __DIR__ . "/../vendor/autoload.php";
 //项目加载 真实项目中参数改为true--用于自动生成项目配置文件(数据库、redis等)
 $bootStrap = new \XYLibrary\Bootstrap\Bootstrap(false);
-$bootStrap->bootstrap();
+$bootStraps = [
+    "test" => "Test"
+];
+$bootStrap->bootstrap($bootStraps);
 //获取项目容器
 $app = $bootStrap->getContainer();
 //注入
-$app->bind("test", function () {
-    return new Test();
+$app->bind("test", function ($app) {
+    return new Test($app);
 });
 //解析类
 $app["test"]->fly();
@@ -19,7 +22,7 @@ echo tap(123, function ($value) {
 
 class Test
 {
-    public function __construct()
+    public function __construct(\XYLibrary\IoC\Container $container)
     {
         echo "class Test init!\r\n";
     }
@@ -27,5 +30,10 @@ class Test
     public function fly()
     {
         echo "this test is success!\r\n";
+    }
+
+    public function register()
+    {
+        echo "register\r\n";
     }
 }
