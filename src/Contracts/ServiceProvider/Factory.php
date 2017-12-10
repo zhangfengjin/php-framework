f4b036e8d9badfaf581eb8a9e48b4cdb30b27601
zhangfengjin <793665131@qq.com>
2017-12-10 14:36:22 +0800

    添加服务注册接口

---
+++ src/Contracts/ServiceProvider/Factory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fengjin1
+ * Date: 2017/12/10
+ * Time: 14:35
+ */
+
+namespace XYLibrary\Contracts\ServiceProvider;
+
+
+interface Factory
+{
+    function register();
+}
